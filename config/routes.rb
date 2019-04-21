05a0fb3cd4e3ca75faf480eb90eb33f5d888e807
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-21 21:48:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :student, only: %i[index show new create edit update]
+  resources :school_classes, only: %i[index show new create edit update]
+  resources :students, only: %i[index show new create edit update]
 end
