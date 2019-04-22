ec6810119a43ba61a4f4574a4f4d205d84607a5a
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-22 03:37:45 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,29 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:title, :room_number))
+    redirect_to student_path(@student)
+  end
 end
